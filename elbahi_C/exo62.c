0ebd69c94a1836f73f1a6f5fd71b5678a4a6667a
Modou Niane <mniane2005@gmail.com>
2025-10-02 21:12:21 +0000

    autre methode de exo62.c

--- elbahi_C/exo62.c
+++ elbahi_C/exo62.c
@@ -1,44 +1,59 @@
 #include<stdio.h>
 int main(){
-    int T[7],element,pos,i;
-    int n=5;
-    puts("Remplissage du tableau");
-    for ( i = 0; i < n; i++)
-    {
-        printf("T[%d]:",i);
-        scanf("%d",&T[i]);
+    int T[7]={1,2,3,4,5};
+    int n=5,position,element;
+    printf("Entrer la position d'insertion (0 a %d): ",n-1);
+    scanf("%d",&position);
+    if(position<0 || position>n){
+        printf("Position invalide!\n");
+        return 1;
     }
-    puts("");
-    printf("Entrer l'element a inserer dans le tableau : ");
+    printf("Entrer l'element a inserer: ");
     scanf("%d",&element);
-    printf("Entrer la position specifique de l'element : ");
-    scanf("%d",&pos);
-    puts("Tableau avant insertion");
-    for ( i = 0; i < n; i++)
-    {
-        printf("%d\t",T[i]);
+    puts("Avant insertion:");
+    for(int i=0;i<n;i++){
+        printf("%d ",T[i]);
     }
-    for ( i = 0; i < n; i++)
-    {
-      if(i==pos){
-        for (int j = n; j > i ;j--)
-        {
-            T[j]=T[j-1];
-        }
-        T[i]=element;
-        (n)++;  
-      }
-      else{
-        printf("\nLa position est invalide");
-        break;
-      }
+    for(int i=n;i>position;i--){
+        T[i]=T[i-1];
     }
-    puts("");
-    puts("Tableau apres insertion");
-    for ( i = 0; i < n; i++)
-    {
-        printf("%d\t",T[i]);
+    T[position]=element;
+    n++;
+    puts("\nApres insertion:");
+    for(int i=0;i<n;i++){
+        printf("%d ",T[i]);
     }
     puts("");
     return 0;
+    /*
+    //autre methode
+    #include <stdio.h>
+
+void insert(int arr[], int *n, int pos, int val) {
+  
+    // Shift elements to the right
+    for (int i = *n; i > pos; i--)
+        arr[i] = arr[i - 1];
+
+    // Insert val at the specified position
+    arr[pos] = val;
+
+    // Increase the current size
+    (*n)++;
+}
+
+int main() {
+    int arr[7] = {10, 20, 30, 40, 50};
+    int n = 5;
+    int pos = 3;
+    int val = 25;
+
+    // Insert the value at the specified position
+    insert(arr, &n, pos, val);
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    return 0;
+}
+    */
 }
